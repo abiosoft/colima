944e490b00ed6c825dc1fe7c21ed6769ea3687e0
Abiola Ibrahim <abiola89@gmail.com>
2021-09-23 22:58:00 +0100

    go rewrite: checkpoint - kubernetes implementation
    Signed-off-by: Abiola Ibrahim <abiola89@gmail.com>

--- environment/container/kubernetes/kubernetes.go
+++ environment/container/kubernetes/kubernetes.go
@@ -5,6 +5,8 @@ import (
 	"github.com/abiosoft/colima/environment"
 	"github.com/abiosoft/colima/environment/container/containerd"
 	"github.com/abiosoft/colima/environment/container/docker"
+	"os"
+	"runtime"
 )
 
 // Name is container runtime name
@@ -22,9 +24,10 @@ func New(host environment.HostActions, guest environment.GuestActions, container
 var _ environment.Container = (*kubernetesRuntime)(nil)
 
 type kubernetesRuntime struct {
-	host    environment.HostActions
-	guest   environment.GuestActions
-	runtime string
+	host             environment.HostActions
+	guest            environment.GuestActions
+	runtime          string
+	newlyProvisioned bool // track first run
 	cli.CommandChain
 }
 
@@ -38,7 +41,7 @@ func (c kubernetesRuntime) isInstalled() bool {
 	return c.guest.Run("command", "-v", "minikube") == nil
 }
 
-func (c kubernetesRuntime) Provision() error {
+func (c *kubernetesRuntime) Provision() error {
 	r := c.Init()
 
 	if c.isInstalled() {
@@ -46,31 +49,78 @@ func (c kubernetesRuntime) Provision() error {
 	}
 
 	r.Stage("provisioning")
-	deps := []string{"conntrack", "socat"}
-
-	switch c.runtime {
-	case containerd.Name:
-		// minikube with containerd still needs docker :( https://github.com/kubernetes/minikube/issues/10908
-		deps = append(deps, "docker.io")
-	case docker.Name:
-		// docker previously installed as part of docker provision
-	}
 
 	// apt install deps
+	r.Stage("installing dependencies")
 	r.Add(func() error {
-		args := append([]string{"sudo", "apt", "install", "-y"}, deps...)
-		return c.guest.Run(args...)
+		// minikube with containerd still needs docker :( https://github.com/kubernetes/minikube/issues/10908
+		return c.guest.Run("sudo", "apt", "install", "-y", "conntrack", "socat", "docker.io")
+	})
+
+	switch c.runtime {
+
+	case containerd.Name:
+		r.Stage("installing " + c.runtime + " dependencies")
+		c.installCrictl(r)
+
+	case docker.Name:
+		// no known dependencies for now
+	}
+
+	// minikube
+	c.installMinikube(r)
+
+	// adding to chain to ensure it executes after successful provision
+	r.Add(func() error {
+		c.newlyProvisioned = true
+		return nil
+	})
+
+	return r.Exec()
+}
+
+func (c kubernetesRuntime) installCrictl(r *cli.ActiveCommandChain) {
+	version := "v1.22.0"
+	downloadPath := "/tmp/crictl.tar.gz"
+	url := "https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-${VERSION}-linux-${ARCH}.tar.gz"
+	url = os.Expand(url, func(s string) string {
+		switch s {
+		case "VERSION":
+			return version
+		case "ARCH":
+			return runtime.GOARCH
+		}
+		return ""
+	})
+
+	r.Add(func() error {
+		return c.guest.Run("curl", "-L", "-o", downloadPath, url)
 	})
 	r.Add(func() error {
-		return c.guest.Run("curl", "-L", "-o", "/tmp/crictl.tar.gz")
+		return c.guest.Run("sudo", "tar", "xvfz", downloadPath, "-C", "/usr/local/bin")
 	})
-	// crictl
-	return nil
+}
+
+func (c kubernetesRuntime) installMinikube(r *cli.ActiveCommandChain) {
+	downloadPath := "/tmp/minikube"
+	url := "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-" + runtime.GOOS
+	r.Add(func() error {
+		return c.guest.Run("curl", "-L", "-o", downloadPath, url)
+	})
+	r.Add(func() error {
+		return c.guest.Run("sudo", "install", downloadPath, "/usr/local/bin/minikube")
+	})
 }
 
 func (c kubernetesRuntime) Start() error {
 	r := c.Init()
 	r.Stage("starting")
+
+	if c.newlyProvisioned {
+		r.Println("NOTE: this is the first startup of kubernetes, it will take a while")
+		r.Println("      but no worries, subsequent startups only take some seconds")
+	}
+
 	r.Add(func() error {
 		return c.guest.Run("minikube", "start", "--driver=none", "--container-runtime", c.runtime)
 	})
