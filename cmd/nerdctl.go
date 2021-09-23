337eaa05c84a1486161607feae244d63bfc54c52
Abiola Ibrahim <abiola89@gmail.com>
2021-09-23 21:12:25 +0100

    go rewrite: checkpoint - fix nerdctl install
    Signed-off-by: Abiola Ibrahim <abiola89@gmail.com>

--- cmd/nerdctl.go
+++ cmd/nerdctl.go
@@ -1,15 +1,21 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/abiosoft/colima/cli"
 	"github.com/abiosoft/colima/runtime/container/containerd"
-	"os"
-
 	"github.com/spf13/cobra"
+	"log"
+	"os"
+	"path/filepath"
+	"text/template"
 )
 
-var nerdctlConf struct {
-	force bool
+var nerdctlCmdArgs struct {
+	force           bool
+	path            string
+	usrBinWriteable bool
 }
 
 // nerdctlCmd represents the nerdctl command
@@ -37,20 +43,80 @@ It is recommended to specify '--' to differentiate from Colima flags.
 	},
 }
 
-// nerdctlLink represents the nerdctl command
-var nerdctlLink = &cobra.Command{
-	Use:   "install",
-	Short: "Install nerdctl binary on the host",
-	Long: `Install nerdctl binary on the host.
-The binary will be installed at /usr/local/bin/nerdctl.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(os.Args)
-		fmt.Println("nerdctl install")
-	},
+// nerdctlLinkFunc represents the nerdctl command
+var nerdctlLinkFunc = func() *cobra.Command {
+	return &cobra.Command{
+		Use:   "install",
+		Short: "install nerdctl binary on the host",
+		Long:  `Install nerdctl binary on the host. The binary will be installed at ` + nerdctlDefaultInstallPath + `.`,
+		PreRun: func(cmd *cobra.Command, args []string) {
+			// check if /usr/local/bin is writeable and no need for sudo
+
+			// if the path is user-specified, ignore.
+			if nerdctlCmdArgs.path != nerdctlDefaultInstallPath {
+				return
+			}
+
+			// attempt writing to the /usr/local/bin
+			tmpFile := filepath.Join(filepath.Dir(nerdctlDefaultInstallPath), "colima.tmp")
+			if err := os.WriteFile(tmpFile, []byte("tmp"), 0777); err == nil {
+				nerdctlCmdArgs.usrBinWriteable = true
+				_ = os.Remove(tmpFile)
+			}
+		},
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if _, err := os.Stat(nerdctlCmdArgs.path); err == nil && !nerdctlCmdArgs.force {
+				return fmt.Errorf("%s exists, use --force to replace", nerdctlCmdArgs.path)
+			}
+
+			t, err := template.New("").Parse(nerdctlScript)
+			if err != nil {
+				return fmt.Errorf("error parsing nerdctl script template: %w", err)
+			}
+			var values = struct{ ColimaApp string }{ColimaApp: os.Args[0]}
+			var buf bytes.Buffer
+			if err := t.Execute(&buf, values); err != nil {
+				return fmt.Errorf("error applying nerdctl script template: %w", err)
+			}
+
+			// /usr/local/bin writeable i.e. sudo not needed
+			// or user-specified install path, we assume user specified path is writeable
+			if nerdctlCmdArgs.usrBinWriteable || nerdctlCmdArgs.path != nerdctlDefaultInstallPath {
+				return os.WriteFile(nerdctlCmdArgs.path, buf.Bytes(), 0755)
+			}
+
+			// sudo is needed for the default path
+			log.Println("/usr/local/bin not writeable, sudo password required to install nerdctl binary")
+			{
+				c := cli.CommandInteractive("sudo", "sh", "-c", "cat > "+nerdctlCmdArgs.path)
+				c.Stdin = &buf
+				if err := c.Run(); err != nil {
+					return err
+				}
+			}
+			// ensure it is executable
+			if err := cli.Command("sudo", "chmod", "+x", nerdctlCmdArgs.path).Run(); err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
 }
+
+const nerdctlDefaultInstallPath = "/usr/local/bin/nerdctl"
+
+const nerdctlScript = `#!/usr/bin/env sh
+
+{{.ColimaApp}} nerdctl -- "$@"
+`
 
 func init() {
 	rootCmd.AddCommand(nerdctlCmd)
+
+	nerdctlLink := nerdctlLinkFunc()
 	nerdctlCmd.AddCommand(nerdctlLink)
-	nerdctlLink.Flags().BoolVarP(&nerdctlConf.force, "force", "f", false, "replace /usr/local/bin/nerdctl (if exists)")
+	nerdctlLink.Flags().BoolVarP(&nerdctlCmdArgs.force, "force", "f", false, "replace "+nerdctlDefaultInstallPath+" (if exists)")
+	nerdctlLink.Flags().StringVarP(&nerdctlCmdArgs.path, "path", "p", nerdctlDefaultInstallPath, "path to install nerdctl binary")
 }
