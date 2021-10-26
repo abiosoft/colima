0bf7e6e3b495e4b8ad411826eacdf488daa4d4d4
Abiola Ibrahim <abiola89@gmail.com>
2021-10-26 09:15:14 +0100

    feat(docker): fixes #38, use docker context to avoid clashes with other servers.
    Signed-off-by: Abiola Ibrahim <abiola89@gmail.com>

---
+++ environment/container/docker/context.go
@@ -0,0 +1,36 @@
+package docker
+
+import (
+	"fmt"
+	"github.com/abiosoft/colima/config"
+)
+
+func (d dockerRuntime) isContextCreated() bool {
+	command := fmt.Sprintf(`docker context ls -q | grep "^%s$"`, config.Profile())
+	return d.host.RunQuiet("sh", "-c", command) == nil
+}
+
+func (d dockerRuntime) setupContext() error {
+	if d.isContextCreated() {
+		return nil
+	}
+
+	profile := config.Profile()
+
+	return d.host.Run("docker", "context", "create", profile,
+		"--description", profile,
+		"--docker", "host=unix://"+socketSymlink(),
+	)
+}
+
+func (d dockerRuntime) useContext() error {
+	return d.host.Run("docker", "context", "use", config.Profile())
+}
+
+func (d dockerRuntime) teardownContext() error {
+	if !d.isContextCreated() {
+		return nil
+	}
+
+	return d.host.Run("docker", "context", "rm", "--force", config.Profile())
+}
