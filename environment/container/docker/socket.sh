62a0014103c4bb316bbcee05a6c5fd0dcc428f20
Tetsuya Shiota <tetsuya.shiota.1231@gmail.com>
2021-11-09 19:38:44 +0900

    Support SSH agent forwarding

--- environment/container/docker/socket.sh
+++ environment/container/docker/socket.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
+
+ssh -p "{{.SSHPort}}" \
+    -l "{{.VMUser}}" \
+    -i ~/.lima/_config/user \
+    -o IdentitiesOnly=yes \
+    -F /dev/null \
+    -o NoHostAuthenticationForLocalhost=yes \
+    "127.0.0.1" \
+    sudo mkdir -p /run/host-services
+
+ssh -p "{{.SSHPort}}" \
+    -l "{{.VMUser}}" \
+    -i ~/.lima/_config/user \
+    -o IdentitiesOnly=yes \
+    -F /dev/null \
+    -o NoHostAuthenticationForLocalhost=yes \
+    "127.0.0.1" \
+    sudo chown {{.VMUser}}:{{.VMUser}} /run/host-services
+
 rm -rf "{{.SocketFile}}"
 ssh -p "{{.SSHPort}}" \
     -l "{{.VMUser}}" \
@@ -7,4 +26,5 @@ ssh -p "{{.SSHPort}}" \
     -F /dev/null \
     -o NoHostAuthenticationForLocalhost=yes \
     -L "{{.SocketFile}}:/var/run/docker.sock" \
+    -R "/run/host-services/ssh-auth.sock:$SSH_AUTH_SOCK" \
     -N "127.0.0.1"
