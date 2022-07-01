8bb1101a861a8b6d2ef6e16aca97a835f65c4f8f
Abiola Ibrahim <git@abiosoft.com>
2022-07-01 07:02:20 +0100

    chore: update nix files (#360)
    * ci: ignore nix files
    
    * chore: add nix flake

---
+++ colima.nix
@@ -0,0 +1,29 @@
+{ pkgs ? import <nixpkgs> }:
+
+with pkgs;
+
+buildGo118Module {
+  name = "colima";
+  pname = "colima";
+  src = ./.;
+  nativeBuildInputs = [ installShellFiles makeWrapper git ];
+  vendorSha256 = "sha256-jDzDwK7qA9lKP8CfkKzfooPDrHuHI4OpiLXmX9vOpOg=";
+
+  # `nix-build` has .git folder but `nix build` does not, this caters for both cases
+  preConfigure = ''
+    export VERSION="$(git describe --tags --always || echo nix-build-at-"$(date +%s)")"
+    export REVISION="$(git rev-parse HEAD || echo nix-unknown)"
+    ldflags="-X github.com/abiosoft/colima/config.appVersion=$VERSION
+              -X github.com/abiosoft/colima/config.revision=$REVISION"
+  '';
+
+  postInstall = ''
+    wrapProgram $out/bin/colima \
+      --prefix PATH : ${lib.makeBinPath [ qemu lima ]}
+    installShellCompletion --cmd colima \
+      --bash <($out/bin/colima completion bash) \
+      --fish <($out/bin/colima completion fish) \
+      --zsh <($out/bin/colima completion zsh)
+  '';
+}
+
--- default.nix
+++ default.nix
@@ -1,21 +1,2 @@
 with import <nixpkgs> { };
-
-buildGo118Module rec {
-  name = "colima";
-  pname = "colima";
-  src = ./.;
-  nativeBuildInputs = [ installShellFiles makeWrapper git ];
-  vendorSha256 = "sha256-jDzDwK7qA9lKP8CfkKzfooPDrHuHI4OpiLXmX9vOpOg=";
-  preConfigure = ''
-    ldflags="-X github.com/abiosoft/colima/config.appVersion=$(git describe --tags --always)
-              -X github.com/abiosoft/colima/config.revision=$(git rev-parse HEAD)"
-  '';
-  postInstall = ''
-    wrapProgram $out/bin/colima \
-      --prefix PATH : ${lib.makeBinPath [ qemu lima ]}
-    installShellCompletion --cmd colima \
-      --bash <($out/bin/colima completion bash) \
-      --fish <($out/bin/colima completion fish) \
-      --zsh <($out/bin/colima completion zsh)
-  '';
-}
+callPackage (import ./colima.nix) { }
