f66e7056504be25022d1506ba3c053387e6df650
Abiola Ibrahim <git@abiosoft.com>
2022-07-01 07:00:04 +0100

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Container runtimes on macOS (and Linux) with minimal setup";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.05";
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem
+    (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = import ./colima.nix { inherit pkgs; };
+        devShell = import ./shell.nix { inherit pkgs; };
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/colima";
+        };
+      }
+    );
+}
