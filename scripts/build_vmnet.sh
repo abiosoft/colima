ecf6c097f39989108a3b2b8dbb7d752c1a044b93
Abiola Ibrahim <git@abiosoft.com>
2022-10-28 15:49:57 +0400

    net: modify vmnet build script

--- scripts/build_vmnet.sh
+++ scripts/build_vmnet.sh
@@ -3,8 +3,7 @@
 set -ex
 
 export DIR_BUILD=$PWD/_build/network
-export DIR_VMNET=$DIR_BUILD/vde_vmnet
-export DIR_VDE=$DIR_BUILD/vde-2
+export DIR_VMNET=$DIR_BUILD/socket_vmnet
 export EMBED_DIR=$PWD/embedded/network
 
 clone() (
@@ -14,53 +13,30 @@ clone() (
 )
 
 mkdir -p "$DIR_BUILD"
-clone https://github.com/lima-vm/vde_vmnet.git "$DIR_VMNET"
-clone https://github.com/virtualsquare/vde-2.git "$DIR_VDE"
+clone https://github.com/lima-vm/socket_vmnet.git "$DIR_VMNET"
 
 move_to_embed_dir() (
-    mkdir -p "$EMBED_DIR"/vmnet/bin "$EMBED_DIR"/vmnet/lib
-    cp "$PREFIX"/bin/vde_vmnet "$EMBED_DIR"/vmnet/bin
-    cp "$PREFIX"/lib/libvdeplug.3.dylib "$EMBED_DIR"/vmnet/lib
-    cd "$EMBED_DIR"/vmnet && tar cvfz "$EMBED_DIR"/vmnet_"${1}".tar.gz bin/vde_vmnet lib/libvdeplug.3.dylib
+    mkdir -p "$EMBED_DIR"/vmnet/bin
+    cp "$PREFIX"/bin/socket_vmnet "$PREFIX"/bin/socket_vmnet_client "$EMBED_DIR"/vmnet/bin
+    cd "$EMBED_DIR"/vmnet && tar cvfz "$EMBED_DIR"/vmnet_"${1}".tar.gz bin/socket_vmnet bin/socket_vmnet_client
     rm -rf "$EMBED_DIR"/vmnet
     sudo rm -rf /opt/colima
 )
 
 build_x86_64() (
-    # shellcheck disable=SC2030
     export PREFIX=/opt/colima
     sudo rm -rf $PREFIX
     sudo mkdir -p $PREFIX
 
-    # shellcheck disable=SC2155
-    export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
-    export CC=$(xcrun --sdk macosx --find clang)
-    export CXX=$(xcrun --sdk macosx --find clang++)
-    export CFLAGS="-arch x86_64 -isysroot $SDKROOT -Wno-error=implicit-function-declaration"
-    export CXXFLAGS=$CFLAGS
-
-    # vde-2
-    (
-        cd "$DIR_VDE"
-        # Dec 12, 2021
-        git checkout 74278b9b7cf816f0356181f387012fdeb6d65b52
-        autoreconf -fis
-        # compile for x86_64
-        ./configure --prefix=$PREFIX --host=x86_64-apple-darwin
-
-        make PREFIX=$PREFIX
-        sudo make PREFIX=$PREFIX install
-        # cleanup
-        make distclean
-    )
-
-    # vde_vmnet
+    # socket_vmnet
     (
         cd "$DIR_VMNET"
+        # pinning to a commit for consistency
+        git checkout c630eb8eeb900de58cd4a487302bd318d0da8abd
         make PREFIX=$PREFIX ARCH=x86_64
         sudo make PREFIX=$PREFIX ARCH=x86_64 install.bin
         # cleanup
-        rm -f vde_vmnet *.o
+        make clean
     )
 
     move_to_embed_dir x86_64
@@ -71,33 +47,15 @@ build_arm64() (
     export PREFIX=/opt/colima
     sudo mkdir -p $PREFIX
 
-    # shellcheck disable=SC2155
-    export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
-    export CC=$(xcrun --sdk macosx --find clang)
-    export CXX=$(xcrun --sdk macosx --find clang++)
-    export CFLAGS="-arch arm64e -isysroot $SDKROOT -Wno-error=implicit-function-declaration"
-    export CXXFLAGS=$CFLAGS
-
-    # vde-2
-    (
-        cd "$DIR_VDE"
-        # Dec 12, 2021
-        git checkout 74278b9b7cf816f0356181f387012fdeb6d65b52
-        autoreconf -fis
-        # compile for arm64
-        ./configure --prefix=$PREFIX --host=arm-apple-darwin
-
-        make PREFIX=$PREFIX
-        sudo make PREFIX=$PREFIX install
-        # cleanup
-        make distclean
-    )
-
-    # vde_vmnet
+    # socket_vmnet
     (
         cd "$DIR_VMNET"
+        # pinning to a commit for consistency
+        git checkout c630eb8eeb900de58cd4a487302bd318d0da8abd
         make PREFIX=$PREFIX ARCH=arm64
         sudo make PREFIX=$PREFIX ARCH=arm64 install.bin
+        # cleanup
+        make clean
     )
 
     move_to_embed_dir arm64
