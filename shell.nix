e552f5371b6bb22d1fd92e22a2ba077b1a2ceeb8
Krzysztof Bezrąk <krzysztof@bezrak.pl>
2023-03-14 11:41:59 +0100

    nix: update flake

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
 pkgs.mkShell {
   # nativeBuildInputs is usually what you want -- tools you need to run
   nativeBuildInputs = with pkgs.buildPackages; [
-    go_1_18
+    go_1_19
     git
     lima
     qemu
