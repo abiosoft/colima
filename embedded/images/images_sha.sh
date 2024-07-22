b67af70b0ebc83d97e6e08693ae9517279eb6294
Abiola Ibrahim <git@abiosoft.com>
2024-07-22 05:24:07 +0100

    core: fix images sha download
    Signed-off-by: Abiola Ibrahim <git@abiosoft.com>

--- embedded/images/images_sha.sh
+++ embedded/images/images_sha.sh
@@ -4,7 +4,7 @@ set -eux
 
 BASE_URL=https://github.com/abiosoft/colima-core/releases/download
 BASE_FILENAME=ubuntu-24.04-minimal-cloudimg
-VERSION=v0.6.10
+VERSION=v0.7.0
 RUNTIMES="none docker containerd incus"
 ARCHS="arm64 amd64"
 
@@ -12,12 +12,12 @@ DIR="$(dirname $0)"
 FILE="${DIR}/images.txt"
 
 # reset output files
-echo >$FILE
+echo -n >$FILE
 
 for arch in ${ARCHS}; do
     for runtime in ${RUNTIMES}; do
         URL="${BASE_URL}/${VERSION}/${BASE_FILENAME}-${arch}-${runtime}.qcow2"
-        SHA="$(curl -sL ${URL}.sha256sum)"
+        SHA="$(curl -sL ${URL}.sha512sum)"
         echo "$arch $runtime ${URL} ${SHA}" >>$FILE
     done
 done
