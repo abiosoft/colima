e9c7e663d2e31fe2dbd75fca622219f0ec105f18
summer <summer@summerdeMac-mini.local>
2024-12-04 19:13:34 +0800

    fix(memory): memorySize is a multiple of 1 megabyte

--- environment/vm/lima/yaml.go
+++ environment/vm/lima/yaml.go
@@ -60,7 +60,7 @@ func newConf(ctx context.Context, conf config.Config) (l limaconfig.Config, err 
 		l.CPUs = &conf.CPU
 	}
 	if conf.Memory > 0 {
-		l.Memory = fmt.Sprintf("%fGiB", conf.Memory)
+		l.Memory = fmt.Sprintf("%dMiB", uint32(conf.Memory*1024)) //memorySize is a multiple of 1 megabyte
 	}
 	if conf.Disk > 0 {
 		l.Disk = fmt.Sprintf("%dGiB", conf.Disk)
