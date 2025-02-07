306b2df2128164fb14b9c8e7d59917ffeb58e92f
jason19970210 <jason19970210@gmail.com>
2025-02-07 20:34:13 +0800

    Revert "Merge branch 'main' of github.com:jason19970210/colima"
    This reverts commit 45e1d1cf820f4c047c3b4890f91a7d7cf36f6b23, reversing
    changes made to 2ccc765ae4b08bc4ca263674c1f003ce300bdfa7.

--- app/app.go
+++ app/app.go
@@ -99,8 +99,7 @@ func (c colimaApp) startWithRuntime(conf config.Config) ([]environment.Container
 func (c colimaApp) Start(conf config.Config) error {
 	ctx := context.WithValue(context.Background(), config.CtxKey(), conf)
 
-	// this tells the full path of current profile being used, instead of misunderstanding it as a template default profile
-	log.Printf("starting with config file: %s\n", config.CurrentProfile().File())
+	log.Println("starting", config.CurrentProfile().DisplayName)
 
 	var containers []environment.Container
 	if !environment.IsNoneRuntime(conf.Runtime) {
